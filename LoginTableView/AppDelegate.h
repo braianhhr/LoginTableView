e4635e4064b62f69a74ed386a85f774b79503a90
Lion User <User@Lions-Mac.local>
2013-03-19 22:32:47 +0000

    Initial Commit

---
+++ LoginTableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoginTableView
+//
+//  Created by Lion User on 3/19/13.
+//  Copyright (c) 2013 Lion User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LoginTableView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LoginTableView
+//
+//  Created by Lion User on 3/19/13.
+//  Copyright (c) 2013 Lion User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
